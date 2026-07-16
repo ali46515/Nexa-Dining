187a78b645cf953b0df9d5129d90c90bcfc4573d
ali46515 <aliarshad.9474@gmail.com>
2026-07-16 09:22:19 +0500

    Update entry point

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import connectMongo from "./config/db.js";
+import errorHandler from "./middlewares/errorHandler.middleware.js";
 
-const app = express()
-const port = 3000
+dotenv.config();
+
+// Connect to MongoDB
+connectMongo();
+
+const app = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to set response headers for CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
+// Routes
+app.use("/api/auth", (await import("./routes/auth.routes.js")).default);
+
+// Middleware to handle errors
+app.use(errorHandler);
+
 
 app.listen(process.env.PORT, () => {
-    console.log(process.env.PORT)
-})
+  console.log(process.env.PORT);
+});
