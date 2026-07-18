5ca94811fba57edcf47a15c3f311151afef32602
ali46515 <aliarshad.9474@gmail.com>
2026-07-18 18:26:35 +0500

    Update entry point

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,17 @@
 import cors from "cors"
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
+import { fileURLToPath } from "url";
 import connectMongo from "./config/db.js";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 
 dotenv.config();
+
+// Get the current file path and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Connect to MongoDB
 connectMongo();
 
@@ -27,9 +34,12 @@ app.use((req, res, next) => {
   next();
 });
 
+// Serve static files from the uploads directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use("/api/auth", (await import("./routes/auth.routes.js")).default);
+app.use('/api/menu', (await import('./routes/menu.routes.js')).default);
 
 // Middleware to handle errors
 app.use(errorHandler);
@@ -37,4 +47,10 @@ app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
